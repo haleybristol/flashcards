34865a53486fcd46839d7250a1d72c2c5aa709a5
haleybritsol and jamesturello <jturello@yahoo.com>
2016-01-11 15:29:42 -0800

    create index css for first 6 flashcards

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+  $(function() {
+   $(".javascript").click(function() {
+      $(".javascript-definition").toggle();
+    });
+    $(".modulo").click(function() {
+      $(".modulo-definition").toggle();
+    });
+    $(".operators").click(function() {
+      $(".operators-definition").toggle();
+    });
+    $(".NaN").click(function() {
+      $(".NaN-definition").toggle();
+    });
+    $(".convention").click(function() {
+      $(".convention-definition").toggle();
+    });
+    $(".plusequal").click(function() {
+      $(".plusequal-definition").toggle();
+    });
+
+
+
+
+  });
